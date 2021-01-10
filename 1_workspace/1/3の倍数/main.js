801da16f20ee66b5ee3622167a2697dd9f336148
Ryo-Nakano <matsuhara.9999ryo@gmail.com>
2021-01-11 01:50:51 +0900

    3で元気になる人_完成

--- 1_workspace/1/3の倍数/main.js
+++ 1_workspace/1/3の倍数/main.js
@@ -1,5 +1,7 @@
 console.log("OK!");
-FizzBuzz(30);
+
+// FizzBuzz(30);
+// FineMan(40);
 
 
 function FizzBuzz(num){
@@ -17,6 +19,20 @@ function FizzBuzz(num){
 
     if(output === ''){ //outputがカラだった時
       output = i;
+    }
+
+    console.log(output); //コンソールに出力
+  }
+}
+
+//3がつくと元気になる関数
+function FineMan(num){
+  for(let i = 1; i <= num; i++){ //numの数だけfor回す
+    let output = i.toString(10);
+
+    if(i % 3 === 0 || output.includes('3')) //3で割り切れるとき or "3"を含んでいたとき
+    {
+      output += '!!!';
     }
 
     console.log(output); //コンソールに出力
